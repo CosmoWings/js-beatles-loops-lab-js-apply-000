a798cd029dc6a7c87bc305b70a21d557866005eb
patrick.pierre000 <patrick.pierre000@gmail.com>
2020-03-13 05:28:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+function theBeatlesPlay(musicianArr,instruArr){
+  let newArr = [];
+  
+  for (let i=0;i<musicianArr.length;i++){
+    let strToPush = `${musicianArr[i]} plays the ${instruArr[i]}.`;
+    newArr.push(strToPush);
+  }
+}
+
+function johnLennonFacts
