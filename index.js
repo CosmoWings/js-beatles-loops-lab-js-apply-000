5f4b5707b5ba780232f46b5eb02cf4ad203370d8
patrick.pierre000 <patrick.pierre000@gmail.com>
2020-03-13 05:59:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicianArr,instruArr){
   let newArr = [];
   
   for (let i=0;i<musicianArr.length;i++){
-    let strToPush = musicianArr[i] + " plays " instruArr[i];
+    let strToPush = musicianArr[i] + " plays " + instruArr[i];
     newArr.push(strToPush);
     return newArr;
   }
